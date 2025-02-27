83b5d60c59144e0cc1415e1545556617a837768e
Melasery <melasery@wesleyan.edu>
2025-02-26 22:05:01 -0500

    Create connection.php
    this code if for the connection between the index.php and the "login.php" (not coded yet).
    
    please name your database "DB1" when create the database that includes the username table on your phpmyadmin.
    
    To confirm that the connection is workin, check for  a connection successful status on your localhost page.

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+    $servername="localhost";
+    $username = "root";
+    $password= "";
+    $db_name= "DB1";
+    $conn= new mysqli($servername, $username, $password, $db_name);
+    if ($conn->connect_error){
+        die("Connection failed".$conn->conenct_error); 
+    }
+    echo "Connection Successul";
+?>
