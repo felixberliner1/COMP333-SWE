a25cc1a77e4cfad501ec47a6801ce13912d31a90
felixberliner1 <61637800+felixberliner1@users.noreply.github.com>
2025-03-01 04:00:05 -0500

    Added table to welcome.php
    I will document the SQL for the 'ratings' table in the comments of the associated issue

--- welcome.php
+++ welcome.php
@@ -7,6 +7,50 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Login Successful!</h1>
+    <h1>Song Ratings</h1>
+<!-- Later: add a log out button and test saying who is logged in-->
+ 
+<!-- Add entry to CRUD table-->
+    <a>Add new entry</a>
+
+<!--CRUD table begins here -->
+    <table>
+    <tr>
+        <th>ID</th>
+        <th>USER</th>
+        <th>SONG</th>
+        <th>ARTIST</th>
+        <th>RATING</th>
+        <th>ACTION</th>
+    </tr>
+        <?php
+            include "connection.php";
+            $sql = "select * from ratings";
+            $result = $conn->query($sql);
+            if(!$result){
+                echo "ERROR";
+            }
+            while($row=$result->fetch_assoc()){
+                echo 
+                "<tr>
+                    <!-- First couple column entries are just data from table -->
+                    <td>$row[id]</td>
+                    <td>$row[username]</td>
+                    <td>$row[song]</td>
+                    <td>$row[artist]</td>
+                    <td>$row[rating]</td>
+
+                    <!-- Next row are the action buttons-->
+                    <td>
+                        <!-- Add classes for CSS and links when functionality is added -->
+                        <a>VIEW</a>
+                        <a>UPDATE</a>
+                        <a>DELETE</a>
+                    </td>
+
+                </tr>";
+            }
+        ?>
+    </table> 
 </body>
 </html>
