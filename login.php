ae8f4583d53eeccea530adbbcdf61553f93e1896
felixberliner1 <61637800+felixberliner1@users.noreply.github.com>
2025-03-07 00:39:27 -0500

    Update login.php
    - removed manual salt stuff from login
    - changed alerts to text that shows up in index

--- login.php
+++ login.php
@@ -6,30 +6,27 @@
         $_SESSION['username'] = $username;
         $password = $_POST['pass'];
 
-        $sql = "SELECT password, salt FROM login WHERE username = ?";
+        $sql = "SELECT password FROM login WHERE username = ?";
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, "s", $username); 
         mysqli_stmt_execute($stmt); 
         mysqli_stmt_store_result($stmt); 
-        mysqli_stmt_bind_result($stmt, $hashed_password, $salt); 
+        mysqli_stmt_bind_result($stmt, $hashed_password); 
         
         if(mysqli_stmt_num_rows($stmt) == 1) {
             mysqli_stmt_fetch($stmt);
     
-            if(password_verify($salt . $password, $hashed_password)) {
+            if(password_verify($password, $hashed_password)) {
+                $_SESSION['loggedin'] = true;
                 header("Location: welcome.php");
                 exit();
             } else {
-                echo  '<script>
-                    window.location.href = "index.php"
-                    alert("Login failed. Invalid username or password.")
-                    </script>';
+                $_SESSION['loginsuccessful'] = false;
+                header("Location: index.php");
             }
         } else {
-            echo  '<script>
-                window.location.href = "index.php"
-                alert("Login failed. Invalid username or password.")
-                </script>';
+            $_SESSION['loginsuccessful'] = false;
+            header("Location: index.php");
         }
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
